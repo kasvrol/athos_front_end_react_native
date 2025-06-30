8a4d17de9559e6760cb9ab961ca8e59621e98e87
kasvrol <dovalekarolina@gmail.com>
2025-06-29 21:56:49 -0300

    ✨ feat: schema de validação de dados para criação de conta do usuário

---
+++ app/utils/zod-schemas/singup.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const singUpFormSchema = z.object({
+  name: z.string().min(3, "O nome deve ter no mínimo 3 caracteres."),
+  cpf: z.string().length(11, "O CPF deve conter exatamente 11 dígitos."),
+  email: z.string().email("Endereço de e-mail inválido."),
+  password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres."),
+  confirmPassword: z.string(),
+  cep: z.string().length(8, "O CEP deve conter exatamente 8 dígitos."),
+})
+.refine((data) => data.password === data.confirmPassword, {
+  message: "As senhas não coincidem.",
+  path: ["confirmPassword"], // Indica qual campo receberá o erro.
+});
+
+export type SignUpFormData = z.infer<typeof singUpFormSchema>;
