eb5326b40822546ecc4f716169813e3a14b4fa32
kasvrol <dovalekarolina@gmail.com>
2025-10-03 00:20:07 -0300

    📘 feat: mock cidades e estados

---
+++ mock/esportes.ts
@@ -0,0 +1,12 @@
+export const esportes = [
+  { id: '1', name: 'Futebol' },
+  { id: '2', name: 'Vôlei' },
+  { id: '3', name: 'Basquete' },
+  { id: '4', name: 'Handebol' },
+  { id: '5', name: 'Rugby' },
+  { id: '6', name: 'Hóquei sobre grama' },
+  { id: '7', name: 'Polo aquático' },
+  { id: '8', name: 'Queimada' },
+  { id: '9', name: 'Críquete' },
+  { id: '10', name: 'Beisebol' },
+]
