b008fbcd3275c920c63ec8714aca89ec0b5a14c1
kasvrol <dovalekarolina@gmail.com>
2025-10-04 13:53:36 -0300

    ✨ feat: componente de loading

---
+++ components/atoms/loading/basketball/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View, StyleSheet, Image } from 'react-native'
+import LottieView from 'lottie-react-native'
+import { Text } from 'tamagui'
+
+export const BasketballLoading = () => {
+  return (
+    <View style={styles.container}>
+      <Text
+        textAlign="center"
+        color="$borderColorFocus"
+        fontSize="$6"
+        fontWeight="600"
+        marginTop={10}
+      >
+        Mais que um app, somos um time!
+      </Text>
+      <Image style={styles.animation} source={require('@/assets/icons/baketball.gif')} />
+      <Text color="$borderColorFocus" fontSize="$5" fontWeight="600" marginTop={10}>
+        Carregando...
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    paddingTop: 300,
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#121212',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    zIndex: 99,
+  },
+  animation: {
+    width: 200,
+    height: 200,
+  },
+})
