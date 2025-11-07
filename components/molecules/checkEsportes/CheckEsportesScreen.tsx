daf3b7d4dec305bddc0067e6d09f12c8f3cd7c12
kasvrol <dovalekarolina@gmail.com>
2025-11-07 00:17:29 -0300

    🐛 fix: layout componente

--- components/molecules/checkEsportes/CheckEsportesScreen.tsx
+++ components/molecules/checkEsportes/CheckEsportesScreen.tsx
@@ -3,44 +3,44 @@ import { Checkbox, XStack, Label, ScrollView, YStack } from 'tamagui'
 import { CheckEsportesViewModel } from './CheckEsportesViewModel'
 import { CheckEsportesScreenProps } from '@/utils/interfaces/esportes'
 
-export default function CheckEsportesScreen({ sportList, selectedSports, setSelectedSports }: CheckEsportesScreenProps) {
+export default function CheckEsportesScreen({
+  sportList,
+  selectedSports,
+  setSelectedSports,
+}: CheckEsportesScreenProps) {
   const { toggleSport } = CheckEsportesViewModel({ setSelectedSports })
 
   return (
-    <YStack maxHeight="100%">
-      <ScrollView>
-        <XStack gap="$3" flexWrap="wrap" justifyContent="flex-start">
-          {sportList.map(sport => {
-            const isChecked = selectedSports.includes(sport.name)
+    <XStack gap="$3" flexWrap="wrap" justifyContent="flex-start" borderColor={'$colorFocus'}>
+      {sportList.map(sport => {
+        const isChecked = selectedSports.includes(sport.name)
 
-            return (
-              <XStack
-                key={sport.id}
-                onPress={() => toggleSport(sport.name)}
-                alignItems="center"
-                gap="$3"
-                paddingVertical="$2"
-                paddingHorizontal="$3"
-                borderRadius="$4"
-                borderWidth={1}
-                borderColor={isChecked ? '$colorFocus' : '$borderColor'}
-                backgroundColor={isChecked ? '$backgroundFocus' : 'transparent'}
-                pressStyle={{ backgroundColor: '$backgroundHover' }}
-              >
-                <Checkbox
-                  id={sport.name}
-                  checked={isChecked}
-                  onCheckedChange={() => toggleSport(sport.name)}
-                  size="$4"
-                >
-                  <Checkbox.Indicator />
-                </Checkbox>
-                <Label htmlFor={sport.name}>{sport.name}</Label>
-              </XStack>
-            )
-          })}
-        </XStack>
-      </ScrollView>
-    </YStack>
+        return (
+          <XStack
+            key={sport.id}
+            onPress={() => toggleSport(sport.name)}
+            alignItems="center"
+            gap="$3"
+            paddingVertical="$2"
+            paddingHorizontal="$3"
+            borderRadius="$4"
+            borderWidth={1}
+            borderColor={isChecked ? '$colorFocus' : '$borderColor'}
+            backgroundColor={isChecked ? '$backgroundFocus' : 'transparent'}
+            pressStyle={{ backgroundColor: '$backgroundHover' }}
+          >
+            <Checkbox
+              id={sport.name}
+              checked={isChecked}
+              onCheckedChange={() => toggleSport(sport.name)}
+              size="$4"
+            >
+              <Checkbox.Indicator />
+            </Checkbox>
+            <Label htmlFor={sport.name}>{sport.name}</Label>
+          </XStack>
+        )
+      })}
+    </XStack>
   )
 }
