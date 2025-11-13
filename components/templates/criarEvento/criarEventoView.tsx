83c67cd3ab5b402646192810c89940e2fc8e6a85
kasvrol <dovalekarolina@gmail.com>
2025-11-13 17:46:53 -0300

    💅 chore: formatação

--- components/templates/criarEvento/criarEventoView.tsx
+++ components/templates/criarEvento/criarEventoView.tsx
@@ -29,7 +29,6 @@ import DateTimePicker from '@react-native-community/datetimepicker'
 import { CriarEventoModelView } from './criarEventoModelView'
 import { BasketballLoading } from '@/components/atoms/loading/basketball'
 
-
 function CriarEventoView() {
   const {
     errors,
@@ -50,247 +49,247 @@ function CriarEventoView() {
 
   return (
     <LayoutComponent>
-        <XStack alignItems="center" marginBottom={8}>
-          <H2 color="$color10" fontFamily="$body" fontWeight="700" textTransform="uppercase">
-            Criar Novo Evento{' '}
-          </H2>
-          <Swords size={30} color="$color10" />
+      <XStack alignItems="center" marginBottom={8}>
+        <H2 color="$color10" fontFamily="$body" fontWeight="700" textTransform="uppercase">
+          Criar Novo Evento{' '}
+        </H2>
+        <Swords size={30} color="$color10" />
+      </XStack>
+
+      {isLoading && <BasketballLoading />}
+
+      <Form gap="$4" paddingBottom="$4">
+        <YStack gap="$1">
+          <Label htmlFor="titulo" color="$color" alignItems="center">
+            <Edit3 size={16} color="$borderColorFocus" /> Título do Evento
+          </Label>
+          <Input
+            id="titulo"
+            height="$9"
+            autoCapitalize="words"
+            placeholder="Ex: Pelada dos Amigos"
+            borderColor={errors.titulo ? '$borderColorError' : '$borderColorFocus'}
+            color="$color"
+            onChangeText={valor => validacaoCampos('titulo', valor)}
+          />
+          {errors.titulo && (
+            <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
+              {errors.titulo}
+            </Text>
+          )}
+        </YStack>
+
+        <YStack gap="$1">
+          <Label htmlFor="descricao" color="$color" alignItems="center">
+            <Info size={16} color="$borderColorFocus" /> Descrição
+          </Label>
+          <TextArea
+            id="descricao"
+            autoCapitalize="words"
+            placeholder="Descreva o evento, regras, nível dos participantes..."
+            borderColor={errors.descricao ? '$borderColorError' : '$borderColorFocus'}
+            numberOfLines={4}
+            height="100"
+            color="$color"
+            onChangeText={valor => validacaoCampos('descricao', valor)}
+          />
+          {errors.descricao && (
+            <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
+              {errors.descricao}
+            </Text>
+          )}
+        </YStack>
+
+        <YStack gap="$2">
+          <Text fontFamily="$body" color="$color" marginBottom="$1">
+            <Bike size={16} color="$borderColorFocus" /> ESPORTES
+          </Text>
+          <YStack
+            borderWidth={1}
+            borderRadius="$4"
+            padding="$3"
+            borderColor={
+              !selectedSports.length || selectedSports.length > 1
+                ? '$borderColorError'
+                : '$borderColorFocus'
+            }
+          >
+            <CheckEsportesScreen
+              sportList={esportes}
+              selectedSports={selectedSports}
+              setSelectedSports={setSelectedSports}
+            />
+          </YStack>
+
+          {(!selectedSports.length || selectedSports.length > 1) && (
+            <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
+              Selecione um esporte!
+            </Text>
+          )}
+        </YStack>
+
+        <YStack gap="$1">
+          <Label htmlFor="cep" color="$color">
+            <MapPin size={16} color="$borderColorFocus" />
+            CEP
+          </Label>
+          <Input
+            id="cep"
+            placeholder="CEP"
+            keyboardType="numeric"
+            borderColor={errors.cep ? '$borderColorError' : '$borderColorFocus'}
+            height="$9"
+            onChangeText={valor => validacaoCampos('cep', valor)}
+            color="$color"
+            maxLength={8}
+          />
+          {errors.cep && (
+            <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
+              {errors.cep}
+            </Text>
+          )}
+        </YStack>
+
+        <YStack gap="$1">
+          <Label htmlFor="endereco" color="$color">
+            Endereço
+          </Label>
+          <Input
+            height="$9"
+            id="endereco"
+            placeholder="Rua, Número"
+            color="$color"
+            borderColor={adress?.endereco ? '$borderColorFocus' : '$borderColorError'}
+            value={adress?.endereco}
+            disabled
+          />
+        </YStack>
+        <XStack alignItems="center" justifyContent="space-between">
+          <YStack gap="$1">
+            <Label htmlFor="numero" color="$color">
+              Número
+            </Label>
+            <Input
+              id="numero"
+              placeholder="Número"
+              height="$9"
+              color="$color"
+              borderColor={errors.numero ? '$borderColorError' : '$borderColorFocus'}
+            />
+            {errors.numero && (
+              <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
+                {errors.numero}
+              </Text>
+            )}
+          </YStack>
+
+          <YStack gap="$1">
+            <Label htmlFor="bairro" color="$color">
+              Bairro
+            </Label>
+            <Input
+              id="bairro"
+              placeholder="Bairro"
+              height="$9"
+              width={200}
+              color="$color"
+              disabled
+              value={adress?.bairro}
+              borderColor={adress?.bairro ? '$borderColorFocus' : '$borderColorError'}
+            />
+          </YStack>
         </XStack>
 
-        {isLoading && <BasketballLoading />}
-
-        <Form gap="$4" paddingBottom="$4">
-          <YStack gap="$1">
-            <Label htmlFor="titulo" color="$color" alignItems="center">
-              <Edit3 size={16} color="$borderColorFocus" /> Título do Evento
-            </Label>
-            <Input
-              id="titulo"
-              height="$9"
-              autoCapitalize="words"
-              placeholder="Ex: Pelada dos Amigos"
-              borderColor={errors.titulo ? '$borderColorError' : '$borderColorFocus'}
-              color="$color"
-              onChangeText={valor => validacaoCampos('titulo', valor)}
-            />
-            {errors.titulo && (
-              <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
-                {errors.titulo}
-              </Text>
-            )}
-          </YStack>
-
-          <YStack gap="$1">
-            <Label htmlFor="descricao" color="$color" alignItems="center">
-              <Info size={16} color="$borderColorFocus" /> Descrição
-            </Label>
-            <TextArea
-              id="descricao"
-              autoCapitalize="words"
-              placeholder="Descreva o evento, regras, nível dos participantes..."
-              borderColor={errors.descricao ? '$borderColorError' : '$borderColorFocus'}
-              numberOfLines={4}
-              height="100"
-              color="$color"
-              onChangeText={valor => validacaoCampos('descricao', valor)}
-            />
-            {errors.descricao && (
-              <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
-                {errors.descricao}
-              </Text>
-            )}
-          </YStack>
-
-          <YStack gap="$2">
-            <Text fontFamily="$body" color="$color" marginBottom="$1">
-              <Bike size={16} color="$borderColorFocus" /> ESPORTES
-            </Text>
-            <YStack
-              borderWidth={1}
-              borderRadius="$4"
-              padding="$3"
-              borderColor={
-                !selectedSports.length || selectedSports.length > 1
-                  ? '$borderColorError'
-                  : '$borderColorFocus'
-              }
-            >
-              <CheckEsportesScreen
-                sportList={esportes}
-                selectedSports={selectedSports}
-                setSelectedSports={setSelectedSports}
-              />
-            </YStack>
-
-            {(!selectedSports.length || selectedSports.length > 1) && (
-              <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
-                Selecione um esporte!
-              </Text>
-            )}
-          </YStack>
-
-          <YStack gap="$1">
-            <Label htmlFor="cep" color="$color">
-              <MapPin size={16} color="$borderColorFocus" />
-              CEP
-            </Label>
-            <Input
-              id="cep"
-              placeholder="CEP"
-              keyboardType="numeric"
-              borderColor={errors.cep ? '$borderColorError' : '$borderColorFocus'}
-              height="$9"
-              onChangeText={valor => validacaoCampos('cep', valor)}
-              color="$color"
-              maxLength={8}
-            />
-            {errors.cep && (
-              <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
-                {errors.cep}
-              </Text>
-            )}
-          </YStack>
-
-          <YStack gap="$1">
-            <Label htmlFor="endereco" color="$color">
-              Endereço
-            </Label>
-            <Input
-              height="$9"
-              id="endereco"
-              placeholder="Rua, Número"
-              color="$color"
-              borderColor={adress?.endereco ? '$borderColorFocus' : '$borderColorError'}
-              value={adress?.endereco}
-              disabled
-            />
-          </YStack>
-          <XStack alignItems="center" justifyContent="space-between">
-            <YStack gap="$1">
-              <Label htmlFor="numero" color="$color">
-                Número
-              </Label>
-              <Input
-                id="numero"
-                placeholder="Número"
-                height="$9"
-                color="$color"
-                borderColor={errors.numero ? '$borderColorError' : '$borderColorFocus'}
-              />
-              {errors.numero && (
-                <Text color="$borderColorError" fontSize="$4" paddingLeft="$1">
-                  {errors.numero}
-                </Text>
-              )}
-            </YStack>
-
-            <YStack gap="$1">
-              <Label htmlFor="bairro" color="$color">
-                Bairro
-              </Label>
-              <Input
-                id="bairro"
-                placeholder="Bairro"
-                height="$9"
-                width={200}
-                color="$color"
-                disabled
-                value={adress?.bairro}
-                borderColor={adress?.bairro ? '$borderColorFocus' : '$borderColorError'}
-              />
-            </YStack>
-          </XStack>
-
-          <YStack gap="$1">
-            <Label htmlFor="cidade" color="$color">
-              Cidade
-            </Label>
-            <Input
-              id="cidade"
-              placeholder="Cidade"
-              height="$9"
-              color="$color"
-              disabled
-              value={adress?.cidade}
-              borderColor={adress?.cidade ? '$borderColorFocus' : '$borderColorError'}
-            />
-          </YStack>
-
-          <XStack gap="$3" justifyContent="space-between">
-            <YStack flex={1} gap="$2">
-              <Label fontFamily="$body" color="$color">
-                Data
-              </Label>
-              <Button
-                icon={Calendar}
-                onPress={() => setShowDatePicker(true)}
-                backgroundColor="$backgroundPress"
-                borderColor={'$borderColor'}
-                pressStyle={{ backgroundColor: '$backgroundHover' }}
-              >
-                <Text color="$borderColorFocus">{date?.toLocaleDateString('pt-BR')}</Text>
-              </Button>
-            </YStack>
-            <YStack flex={1} gap="$2">
-              <Label fontFamily="$body" color="$color">
-                Hora
-              </Label>
-              <Button
-                icon={Clock}
-                onPress={() => setShowTimePicker(true)}
-                backgroundColor="$backgroundPress"
-                borderColor={'$borderColor'}
-                pressStyle={{ backgroundColor: '$backgroundHover' }}
-              >
-                <Text color="$borderColorFocus">
-                  {hour?.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' })}
-                </Text>
-              </Button>
-            </YStack>
-          </XStack>
-
-          {showDatePicker && (
-            <DateTimePicker
-              value={date}
-              mode="date"
-              display="default"
-              onChange={onDateChange}
-              minimumDate={new Date()}
-            />
-          )}
-
-          {showTimePicker && (
-            <DateTimePicker value={hour} mode="time" display="default" onChange={onTimeChange} />
-          )}
-
-          <YStack gap="$1">
-            <Label htmlFor="valor" color="$color">
-              <DollarSign size={16} color="$color5" /> Valor (R$ - opcional)
-            </Label>
-            <Input
-              id="valor"
-              placeholder="0,00"
-              keyboardType="numeric"
-              borderColor={'$borderColorFocus'}
-              height="$9"
-              color="$color"
-            />
-          </YStack>
-
-          <Button
+        <YStack gap="$1">
+          <Label htmlFor="cidade" color="$color">
+            Cidade
+          </Label>
+          <Input
+            id="cidade"
+            placeholder="Cidade"
             height="$9"
-            // onPress={()=>handleSubmit()}
-            disabled={isLoading}
-            backgroundColor={isLoading ? '$backgroundPress' : '$color9'}
-            color={isLoading ? '$color' : '$background'}
-            fontFamily="$body"
-            fontWeight="bold"
-            icon={isLoading ? <Spinner color={isLoading ? '$color' : '$background'} /> : undefined}
-            pressStyle={{ backgroundColor: '$color11' }}
-          >
-            {isLoading ? 'Criando...' : 'CRIAR EVENTO'}
-          </Button>
-        </Form>
+            color="$color"
+            disabled
+            value={adress?.cidade}
+            borderColor={adress?.cidade ? '$borderColorFocus' : '$borderColorError'}
+          />
+        </YStack>
+
+        <XStack gap="$3" justifyContent="space-between">
+          <YStack flex={1} gap="$2">
+            <Label fontFamily="$body" color="$color">
+              Data
+            </Label>
+            <Button
+              icon={Calendar}
+              onPress={() => setShowDatePicker(true)}
+              backgroundColor="$backgroundPress"
+              borderColor={'$borderColor'}
+              pressStyle={{ backgroundColor: '$backgroundHover' }}
+            >
+              <Text color="$borderColorFocus">{date?.toLocaleDateString('pt-BR')}</Text>
+            </Button>
+          </YStack>
+          <YStack flex={1} gap="$2">
+            <Label fontFamily="$body" color="$color">
+              Hora
+            </Label>
+            <Button
+              icon={Clock}
+              onPress={() => setShowTimePicker(true)}
+              backgroundColor="$backgroundPress"
+              borderColor={'$borderColor'}
+              pressStyle={{ backgroundColor: '$backgroundHover' }}
+            >
+              <Text color="$borderColorFocus">
+                {hour?.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' })}
+              </Text>
+            </Button>
+          </YStack>
+        </XStack>
+
+        {showDatePicker && (
+          <DateTimePicker
+            value={date}
+            mode="date"
+            display="default"
+            onChange={onDateChange}
+            minimumDate={new Date()}
+          />
+        )}
+
+        {showTimePicker && (
+          <DateTimePicker value={hour} mode="time" display="default" onChange={onTimeChange} />
+        )}
+
+        <YStack gap="$1">
+          <Label htmlFor="valor" color="$color">
+            <DollarSign size={16} color="$color5" /> Valor (R$ - opcional)
+          </Label>
+          <Input
+            id="valor"
+            placeholder="0,00"
+            keyboardType="numeric"
+            borderColor={'$borderColorFocus'}
+            height="$9"
+            color="$color"
+          />
+        </YStack>
+
+        <Button
+          height="$9"
+          // onPress={()=>handleSubmit()}
+          disabled={isLoading}
+          backgroundColor={isLoading ? '$backgroundPress' : '$color9'}
+          color={isLoading ? '$color' : '$background'}
+          fontFamily="$body"
+          fontWeight="bold"
+          icon={isLoading ? <Spinner color={isLoading ? '$color' : '$background'} /> : undefined}
+          pressStyle={{ backgroundColor: '$color11' }}
+        >
+          {isLoading ? 'Criando...' : 'CRIAR EVENTO'}
+        </Button>
+      </Form>
     </LayoutComponent>
   )
 }
