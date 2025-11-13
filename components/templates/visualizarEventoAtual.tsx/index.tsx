31ace5dca5c7406023cc8283e0a3a2f718a05e0d
kasvrol <dovalekarolina@gmail.com>
2025-11-13 17:44:58 -0300

    ✨ feat: componente de visualizar evento que ocorreu dentro de 24h

---
+++ components/templates/visualizarEventoAtual.tsx/index.tsx
@@ -0,0 +1,66 @@
+import LayoutDefault from '@/components/atoms/layoutDefault'
+import { BasketballLoading } from '@/components/atoms/loading/basketball'
+import { ListarParticipantes } from '@/components/molecules/listarParticipantes'
+import { EventScreen } from '@/components/organisms/evento'
+import { AvaliarOrganizador } from '@/components/organisms/listarOrganizador'
+import { eventosCompletos, participantesEvento } from '@/mock/eventosEsportivos'
+import { useUserStore } from '@/store/UserStore'
+import { EventoType, StatusEvento } from '@/utils/interfaces/eventos'
+import React, { useEffect, useState } from 'react'
+import { YStack} from 'tamagui'
+
+const eventoVazio: EventoType = {
+  titulo: '',
+  descricao: '',
+  esporte: '',
+  endereco: '',
+  valor: 0,
+  data: '',
+  horario: '',
+  qtdVagas: 0,
+  user: null,
+  organizador: {
+    idOrganizador: '',
+    nomeOrganizador: '',
+    avaliacaoOrganizador: 0.0,
+  },
+}
+
+
+export default function VisualizarEventoAtual() {
+  const [dadosEvento, setDadosEvento] = useState<EventoType>(eventoVazio)
+  const [participantes, setParticipantes] = useState<any[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isParticipant, setIsParticipant] = useState<boolean>(true)
+  const [error, setError] = useState<string>('')
+  const user = useUserStore(state => state.user)
+
+  useEffect(() => {
+    setDadosEvento(eventosCompletos[0])
+    setParticipantes(participantesEvento)
+    const usuarioJaEhParticipante = participantesEvento.find(
+      participante => participante.idParticipante === user?.id,
+    )
+    setIsParticipant(usuarioJaEhParticipante?.nomeParticipante ? true : false)
+  }, [])
+  return (
+    <LayoutDefault>
+      {isLoading && <BasketballLoading />}
+      <YStack paddingBottom={'$4'}>
+        <EventScreen dadosEvento={dadosEvento} statusEvento={StatusEvento.OCORRENDO} />
+        {user?.id === dadosEvento.organizador.idOrganizador ? (
+          <ListarParticipantes
+            participants={participantesEvento}
+            statusEvento={StatusEvento.OCORRENDO}
+          />
+        ) : (
+          <AvaliarOrganizador
+            nomeOrganizador={dadosEvento.organizador.nomeOrganizador}
+            avaliacaoOrganizador={dadosEvento.organizador.avaliacaoOrganizador}
+            idOrganizador={dadosEvento.organizador.idOrganizador}
+          />
+        )}
+      </YStack>
+    </LayoutDefault>
+  )
+}
