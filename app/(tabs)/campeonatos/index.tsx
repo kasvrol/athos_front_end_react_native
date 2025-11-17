71bfb1e7f511d80318f4d9d4c4b2618b744c6dfc
kasvrol <dovalekarolina@gmail.com>
2025-11-16 22:27:49 -0300

    ✨ feat: componente de listar campeonatos

--- app/(tabs)/campeonatos/index.tsx
+++ app/(tabs)/campeonatos/index.tsx
@@ -1,17 +1,7 @@
-// app/(tabs)/championships.tsx
-import React from 'react'
-import { YStack, H2, Text } from 'tamagui'
-import LayoutComponent from '@/components/atoms/layout'
+import ListarCampeonatos from '@/components/templates/listarCampeonatos'
 
 export default function ChampionshipsScreen() {
   return (
-    <LayoutComponent>
-      <YStack flex={1} alignItems="center" justifyContent="center" padding="$4" space="$3">
-        <H2 color="$color10">Campeonatos</H2>
-        <Text color="$color" fontSize="$5" textAlign="center">
-          Esta tela exibirá os campeonatos disponíveis. (Conteúdo a implementar)
-        </Text>
-      </YStack>
-    </LayoutComponent>
+    <ListarCampeonatos/>
   )
 }
---
+++ components/templates/listarCampeonatos/index.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { H2, ScrollView, Text, XStack, YStack } from 'tamagui';
+import LayoutComponent from '@/components/atoms/layout';
+import { mockCampeonatos } from '@/mock/campeonatos';
+import { BasketballLoading } from '@/components/atoms/loading/basketball'; 
+import HeaderEventosCampeonatosView from '@/components/organisms/headerEventosCampeonatos'; 
+import { CardCampeonato } from '@/components/organisms/cardCampeonato';
+import { Trophy } from '@tamagui/lucide-icons';
+import { ExternalPathString, RelativePathString } from 'expo-router';
+import { Campeonato } from '@/utils/interfaces/campeonatos';
+
+
+export default function ListarCampeonatos() {
+  const [campeonatos, setCampeonatos] = useState<Campeonato[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    setIsLoading(true);
+      setError('');
+      setCampeonatos(mockCampeonatos); 
+          setIsLoading(false);
+  }, []);
+
+  return (
+    <LayoutComponent>
+      <YStack flex={1}>
+        <XStack
+        justifyContent="center"
+        alignItems="center"
+        gap={'$3'}
+        marginBottom={20}
+      >
+        <H2 color="$color10" fontFamily={'$body'} fontWeight={'700'} textAlign="center">
+          Campeonatos
+        </H2>
+        <Trophy color="$color10" />
+      </XStack>
+        <HeaderEventosCampeonatosView
+          routerButton={'/(tabs)/campeonatos/criar' as RelativePathString | ExternalPathString}
+          titleButton="CRIAR CAMPEONATO"
+        />
+
+        {isLoading && <BasketballLoading />}
+
+        {error && <Text color="$borderColorError">{error}</Text>}
+
+        {!isLoading && !error && (
+          <ScrollView>
+            {campeonatos && campeonatos.length > 0 ? (
+              campeonatos.map(camp => <CardCampeonato key={camp.id} campeonato={camp} />)
+            ) : (
+              <Text color="$color" fontSize="$5" textAlign="center">
+                Nenhum campeonato encontrado.
+              </Text>
+            )}
+          </ScrollView>
+        )}
+      </YStack>
+    </LayoutComponent>
+  );
+}
