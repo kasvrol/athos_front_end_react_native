f4e7146f99e1822242cae9669129037609b28410
kasvrol <dovalekarolina@gmail.com>
2025-11-16 21:53:17 -0300

    ✨ feat: componente de tabela de classificacao

---
+++ components/organisms/classificacaoTabela/index.tsx
@@ -0,0 +1,21 @@
+import { XStack, YStack,Text, Separator } from "tamagui";
+
+const ClassificacaoTabela = ({ classificacao }) => (
+  <YStack width="100%" gap="$2">
+    <XStack paddingHorizontal="$2">
+      <Text flex={3} fontWeight="bold">Equipe</Text>
+      <Text flex={1} textAlign="center" fontWeight="bold">P</Text>
+      <Text flex={1} textAlign="center" fontWeight="bold">V</Text>
+      <Text flex={1} textAlign="center" fontWeight="bold">J</Text>
+    </XStack>
+    <Separator />
+    {classificacao.map((item, index) => (
+      <XStack key={item.equipe.id} paddingHorizontal="$2" backgroundColor={index % 2 === 0 ? '$backgroundPress' : 'transparent'} paddingVertical="$2">
+        <Text flex={3}>{item.equipe.nome}</Text>
+        <Text flex={1} textAlign="center">{item.pontos}</Text>
+        <Text flex={1} textAlign="center">{item.vitorias}</Text>
+        <Text flex={1} textAlign="center">{item.jogos}</Text>
+      </XStack>
+    ))}
+  </YStack>
+);
