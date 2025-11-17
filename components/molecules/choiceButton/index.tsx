3be68eecdc73f8db8bd023290b6566ef6585e927
kasvrol <dovalekarolina@gmail.com>
2025-11-16 23:25:21 -0300

    🐛 fix: ajuste na lógica da tela e modal de confirmação de exclusão de evento

--- components/molecules/choiceButton/index.tsx
+++ components/molecules/choiceButton/index.tsx
@@ -2,7 +2,7 @@ import { CanceledButton } from '@/components/atoms/buttons/canceledButton'
 import { ConfirmButton } from '@/components/atoms/buttons/confirmButton'
 import { Edit3, UserMinus2, UserPlus2 } from '@tamagui/lucide-icons'
 import { router } from 'expo-router'
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import { YStack } from 'tamagui'
 
 interface ChoiceButtonProps {
@@ -20,37 +20,64 @@ export const ChoiceButton = ({
   setIsParticipant,
   setIsLoading,
 }: ChoiceButtonProps) => {
+  const [open, setOpen] = useState<boolean>(false)
+
+  const ConfirmarDeletarEvento = () => {
+    return (
+      <YStack>
+        Deseja realmente excluir o evento?
+        <YStack>
+          <ConfirmButton
+            titleButton={'NÃO'}
+            functionButton={() => setOpen(!open)}
+            icon={<Edit3 />}
+          />
+          <CanceledButton
+            disabled={false}
+            width={'100%'}
+            message={'SIM'}
+            display={'flex'}
+            onPress={() => router.back()}
+          />
+        </YStack>
+      </YStack>
+    )
+  }
+
   const onChangeParticipant = () => {
     setIsLoading(true)
     if (isParticipant) {
       //const postParticipante = postParticipant(idEvento, idParticipant)
-      setIsParticipant(true)
+      setIsParticipant(false)
     } else {
       //const postParticipante = postParticipant(idEvento, idParticipant)
-      setIsParticipant(false)
+      setIsParticipant(true)
     }
 
     setIsLoading(false)
   }
 
-  if (idUser === idCriador) {
+  const ItNotIsParticipant = () => {
     return (
-      <YStack>
+      <YStack width={'100%'} gap={'$4'} paddingVertical={'$4'}>
         <ConfirmButton
-          titleButton={'EDITAR EVENTO'}
-          functionButton={() => router.push('/criarEvento')}
-          icon={<Edit3 />}
+          titleButton={'PARTICIPAR'}
+          functionButton={() => onChangeParticipant()}
+          icon={<UserPlus2 />}
         />
+
         <CanceledButton
           disabled={false}
           width={'100%'}
-          message={'DELETAR EVENTO'}
+          message={'VOLTAR'}
           display={'flex'}
-          onPress={() => router.push('/criarEvento')}
+          onPress={() => router.push('/(tabs)/menu/historico')}
         />
       </YStack>
     )
-  } else if (isParticipant) {
+  }
+
+  const ItIsParticipant = () => {
     return (
       <YStack width={'100%'} gap={'$4'} paddingVertical={'$4'}>
         <ConfirmButton
@@ -64,26 +91,42 @@ export const ChoiceButton = ({
           width={'100%'}
           message={'VOLTAR'}
           display={'flex'}
-          onPress={() => router.back()}
+          onPress={() => router.push('/(tabs)/menu/historico')}
         />
       </YStack>
     )
   }
-  return (
-    <YStack width={'100%'} gap={'$4'} paddingVertical={'$4'}>
-      <ConfirmButton
-        titleButton={'PARTICIPAR'}
-        functionButton={() => onChangeParticipant()}
-        icon={<UserPlus2 />}
-      />
 
-      <CanceledButton
-        disabled={false}
-        width={'100%'}
-        message={'VOLTAR'}
-        display={'flex'}
-        onPress={() => router.back()}
-      />
+  const ItIsOwner = () => {
+    return (
+      <YStack width={'100%'} gap={'$4'} paddingVertical={'$4'}>
+        <ConfirmButton
+          titleButton={'EDITAR EVENTO'}
+          functionButton={() => router.push('/(tabs)/eventos/criarEvento')}
+          icon={<Edit3 />}
+        />
+        <CanceledButton
+          disabled={false}
+          width={'100%'}
+          message={'DELETAR EVENTO'}
+          display={'flex'}
+          onPress={() => setOpen(!open)}
+        />
+      </YStack>
+    )
+  }
+  console.log(idUser == idCriador)
+  console.log(idUser)
+  console.log(idCriador)
+
+  return (
+    <YStack>
+      {open && ConfirmarDeletarEvento()}
+      {idUser === idCriador
+        ? ItIsOwner()
+        : isParticipant
+          ? ItNotIsParticipant()
+          : ItIsParticipant()}
     </YStack>
   )
 }
